9724053ce804d178ab8e67e611dd78c349354eae
Tucker <barbct5@gmail.com>
2017-12-29 11:06:14 +0000

    more model swim_membership

--- test/property_test/prop_swim_membership.erl
+++ test/property_test/prop_swim_membership.erl
@@ -30,8 +30,8 @@
 -export([precondition/2]).
 
 -export([alive/2]).
--export([suspect/3]).
--export([faulty/3]).
+-export([suspect/2]).
+-export([faulty/2]).
 -export([members/0]).
 
 -export([start_link/1]).
@@ -51,22 +51,35 @@
          }).
 
 g_local_member(State) ->
-    exactly(State#state.me).
+    {exactly(State#state.me), exactly(State#state.incarnation)}.
 
 g_non_local_member(State) ->
-    ?LET({Member, _CurrentStatus, _CurrentInc},
-         oneof(State#state.members),
-         Member).
+    ?LET(IncFactor, range(-1, 1),
+         ?LET({Member, _CurrentStatus, CurrentInc},
+              oneof(State#state.members),
+              {Member, CurrentInc + IncFactor})).
 
 g_existing_member(State) ->
     oneof([g_local_member(State), g_non_local_member(State)]).
 
+g_existing_suspected_member(State) ->
+    ?LET({Member, _CurrentStatus, CurrentInc},
+         ?SUCHTHATMAYBE({_Member, CurrentStatus, _CurrentInc},
+                        oneof(State#state.members),
+                        CurrentStatus =:= suspect),
+         {Member, CurrentInc}).
+
+g_suspected_member(State) ->
+    frequency([{1, {g_member(State), g_incarnation()}}] ++
+                  [{1, g_existing_member(State)} || State#state.members =/= []] ++
+                  [{2, g_existing_suspected_member(State)} || State#state.members =/= []]).
+
 g_suspecting_member(State) ->
-    frequency([{5, g_member(State)}, {1, local}]).
+    frequency([{5, g_member(State)}, {2, local}]).
 
 g_member(State) ->
     frequency([{1, g_member()}] ++
-                  [{3, g_existing_member(State)} || State#state.members /= []]).
+                  [{3, g_existing_member(State)} || State#state.members =/= []]).
 
 initial_state() ->
     #state{me = {{127,0,0,1},5000}}.
@@ -75,12 +88,16 @@ command(State) ->
     oneof([
            {call, ?MODULE, alive, [g_member(State), g_incarnation()]},
            {call, ?MODULE, suspect,
-            [g_member(State), g_incarnation(), g_suspecting_member(State)]},
+            [g_suspected_member(State), g_suspecting_member(State)]},
            {call, ?MODULE, faulty,
-            [g_member(State), g_incarnation(), g_suspecting_member(State)]},
+            [g_suspected_member(State), g_suspecting_member(State)]},
            {call, ?MODULE, members, []}
           ]).
 
+precondition(#state{members = []}, {call, ?MODULE, suspect, _}) ->
+    false;
+precondition(#state{members = []}, {call, ?MODULE, faulty, _}) ->
+    false;
 precondition(_State, {call, ?MODULE, suspect, [Member, _Inc, Member]}) ->
     false;
 precondition(_State, {call, ?MODULE, faulty, [Member, _Inc, Member]}) ->
@@ -93,9 +110,9 @@ postcondition(State, {call, ?MODULE, members, []}, Members) ->
                      ordsets:from_list(Members)) =:= [];
 postcondition(_State, {call, ?MODULE, alive, [_Member, _Inc]}, ok) ->
     true;
-postcondition(_State, {call, ?MODULE, suspect, [_Member, _Inc, _From]}, ok) ->
+postcondition(_State, {call, ?MODULE, suspect, [{_Member, _Inc}, _From]}, ok) ->
     true;
-postcondition(_State, {call, ?MODULE, faulty, [_Member, _Inc, _From]}, ok) ->
+postcondition(_State, {call, ?MODULE, faulty, [{_Member, _Inc}, _From]}, ok) ->
     true.
 
 next_state(State, _V, {call, ?MODULE, members, []}) ->
@@ -123,7 +140,7 @@ next_state(State, _V, {call, ?MODULE, alive, [Member, Incarnation]}) ->
                     State
             end
     end;
-next_state(State, _V, {call, ?MODULE, suspect, [Member, Incarnation, _From]}) ->
+next_state(State, _V, {call, ?MODULE, suspect, [{Member, Incarnation}, _From]}) ->
     case State#state.me =:= Member of
         true ->
             case Incarnation >= State#state.incarnation of
@@ -144,7 +161,7 @@ next_state(State, _V, {call, ?MODULE, suspect, [Member, Incarnation, _From]}) ->
                     State
             end
     end;
-next_state(State, _V, {call, ?MODULE, faulty, [Member, Incarnation, _From]}) ->
+next_state(State, _V, {call, ?MODULE, faulty, [{Member, Incarnation}, _From]}) ->
     case State#state.me =:= Member of
         true ->
             case Incarnation >= State#state.incarnation of
@@ -157,7 +174,7 @@ next_state(State, _V, {call, ?MODULE, faulty, [Member, Incarnation, _From]}) ->
             case lists:keytake(Member, 1, State#state.members) of
                 false ->
                     State;
-                {value, {Member, _Status, CurrentIncarnation}, Rest}
+                {value, {Member, suspect, CurrentIncarnation}, Rest}
                   when Incarnation >= CurrentIncarnation ->
                     State#state{members = Rest};
                 _ ->
@@ -179,10 +196,10 @@ prop_membership() ->
 alive(Member, Incarnation) ->
     gen_server:call(?MODULE, {alive, Member, Incarnation}).
 
-suspect(Member, Incarnation, From) ->
+suspect({Member, Incarnation}, From) ->
     gen_server:call(?MODULE, {suspect, Member, Incarnation, From}).
 
-faulty(Member, Incarnation, From) ->
+faulty({Member, Incarnation}, From) ->
     gen_server:call(?MODULE, {faulty, Member, Incarnation, From}).
 
 members() ->
