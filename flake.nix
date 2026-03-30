85f6be7d406811c939e2b423f54a49c264014893
Tucker Barbour <tucker.barbour@gmail.com>
2026-03-30 10:48:32 +0100

    chore: add nix flake for erlang and rebar3 dev environment

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { nixpkgs, ... }:
+    let
+      forAllSystems = nixpkgs.lib.genAttrs [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+    in
+    {
+      devShells = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          erlang = pkgs.beam.interpreters.erlang_27;
+          beamPkgs = pkgs.beam.packages.erlang_27;
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [
+              erlang
+              beamPkgs.rebar3
+            ];
+          };
+        }
+      );
+    };
+}
